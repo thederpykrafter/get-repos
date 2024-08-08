6eeb02cd7e528670ec83efcf671227ea186a0648
thederpykrafter <thederpykrafter@gmail.com>
2024-08-07 21:52:53 -0400

    shorten curr_repo

--- get-repos.sh
+++ get-repos.sh
@@ -6,7 +6,9 @@ repos=$(find ~ -name ".git" | grep -v ".local" | grep -v ".cache")
 
 for repo in $repos; do
 	cd $repo && cd ../
-	echo -e "\e[94mRepo\e[m:"$PWD
+
+  curr_repo=$(echo $repo | sed "s/\/data\/data\/com.termux\/files\/home\///" | sed "s/\.git$//")
+	echo -e "\e[94mFound repo\e[m:"$curr_repo
 	lazygit
 done
 
