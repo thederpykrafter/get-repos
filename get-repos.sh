39fdac33a15e67eceddc54386cd7030fc8007322
thederpykrafter <thederpykrafter@gmail.com>
2024-08-08 20:30:25 -0400

    remove windows prefix too

--- get-repos.sh
+++ get-repos.sh
@@ -7,7 +7,7 @@ repos=$(find ~ -name ".git" | grep -v ".local" | grep -v ".cache")
 for repo in $repos; do
 	cd $repo && cd ../
 
-  curr_repo=$(echo $repo | sed "s/\/data\/data\/com.termux\/files\/home\///" | sed "s/\.git$//")
+  curr_repo=$(echo $repo | sed "s/\/data\/data\/com.termux\/files\/home\///" | sed "s/\/home\/thederpykrafter\///" | sed "s/\.git$//")
 	echo -e "\e[94mFound repo\e[m:"$curr_repo
 	lazygit
 done
